c04555f7f991428cc8e20fac7de66d263c9e4f0c
kelcyw <kelcy.w.wang@gmail.com>
2022-11-12 19:17:29 -0800

    added functionality for opening team view dialog

--- src/main/ui/gui/TeamMakerAppGUI.java
+++ src/main/ui/gui/TeamMakerAppGUI.java
@@ -1,6 +1,8 @@
 package ui.gui;
 
+import model.Hero;
 import model.HeroType;
+import model.Team;
 import model.TeamList;
 import persistence.JsonReader;
 import persistence.JsonWriter;
@@ -10,12 +12,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 // The main application class that contains information for the main menu (GUI version)
 // JSON-related code is modelled after JsonSerializationDemo
-// GUI-related code is based on code from SimpleDrawingPlayer
+// GUI-related code is partially based on code from SimpleDrawingPlayer
 
 public class TeamMakerAppGUI {
 
@@ -33,8 +37,8 @@ public class TeamMakerAppGUI {
     private static final int TEAMVIEW_BUTTON_X = 37;
 
     private JFrame frame;
-    private JDialog teamView;
-    private JPanel panel;
+    private JPanel mainPanel;
+    private JPanel teamPanel;
 
     // EFFECTS: runs the team maker application (with GUI)
     public TeamMakerAppGUI() {
@@ -43,12 +47,17 @@ public class TeamMakerAppGUI {
         runTeamMaker();
     }
 
-    // EFFECTS: initializes graphics, interaction, heroTypes
+    // EFFECTS: initializes graphics, heroTypes
     private void runTeamMaker() {
-        initializeGraphics();
-        // initializeInteraction();
         availableHeroTypes = new ArrayList<>();
         initializeHeroTypes();
+
+        savedTeams =  new TeamList();
+        Team team1 = new Team("Team 1");
+        team1.addHeroToTeam(new Hero("Dismas", availableHeroTypes.get(0)));
+        savedTeams.addTeam(team1);
+
+        initializeGraphics();
     }
 
     // MODIFIES: this
@@ -62,15 +71,22 @@ public class TeamMakerAppGUI {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
-        panel = new JPanel();
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        teamPanel = new JPanel();
+        teamPanel.setLayout(new FlowLayout());
+        addTeamButtons(teamPanel);
+        teamPanel.setVisible(true);
 
         JToolBar toolbar = new JToolBar();
-        panel.add(toolbar);
-
+        toolbar.setFloatable(false);
         addToolButtons(toolbar);
-        addMenuButtons(panel);
+        mainPanel.add(toolbar, BorderLayout.PAGE_START);
+        mainPanel.add(teamPanel, BorderLayout.CENTER);
 
-        frame.getContentPane().add(panel);
+        frame.getContentPane().add(mainPanel);
+        mainPanel.setVisible(true);
     }
 
     // MODIFIES: this
@@ -83,29 +99,30 @@ public class TeamMakerAppGUI {
     }
 
     // MODIFIES: panel
-    // EFFECTS: adds menu buttons to panel
-    private void addMenuButtons(JPanel panel) {
-        JButton t1Button = new JButton("Team 1");
-        t1Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                openTeamView(e, "Team 1");
-            }
-        });
-        JButton t2Button = new JButton("Team 2");
-        t2Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                openTeamView(e, "Team 2");
-            }
-        });
-        panel.add(t1Button);
-        panel.add(t2Button);
+    // EFFECTS: adds team buttons to panel
+    private void addTeamButtons(JPanel panel) {
+        for (Team team : savedTeams.getSavedTeams()) {
+            JButton button = new JButton(team.getTeamName());
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    openTeamView(e, team);
+                }
+            });
+            panel.add(button, BorderLayout.CENTER);
+        }
     }
 
     // MODIFIES: toolbar
     // EFFECTS: adds tool buttons for toolbar
     private void addToolButtons(JToolBar tb) {
+        initializeAddButton(tb);
+        initializeDelButton(tb);
+        initializeLoadButton(tb);
+        initializeSaveButton(tb);
+    }
+
+    private void initializeAddButton(JToolBar tb) {
         JButton buttonNew = new JButton("New Team");
         buttonNew.addActionListener(new ActionListener() {
             @Override
@@ -113,69 +130,127 @@ public class TeamMakerAppGUI {
                 addTeam(e);
             }
         });
+        tb.add(buttonNew);
+    }
 
+    private void initializeDelButton(JToolBar tb) {
         JButton buttonDel = new JButton("Delete Team");
-        buttonNew.addActionListener(new ActionListener() {
+        buttonDel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 removeTeam(e);
             }
         });
-        JButton buttonLoad = new JButton("Load Teams");
-        JButton buttonSave = new JButton("Save Teams");
-        tb.add(buttonNew);
         tb.add(buttonDel);
+    }
+
+    public void initializeLoadButton(JToolBar tb) {
+        JButton buttonLoad = new JButton("Load Teams");
+        buttonLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadTeams(e);
+            }
+        });
         tb.add(buttonLoad);
+
+    }
+
+    public void initializeSaveButton(JToolBar tb) {
+        JButton buttonSave = new JButton("Save Teams");
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveTeams(e);
+            }
+        });
         tb.add(buttonSave);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens up team view for clicked team button
+    private void openTeamView(ActionEvent e, Team team) {
+        JDialog teamView = new JDialog();
+        teamView.setSize(new Dimension(TEAMVIEW_WIDTH,TEAMVIEW_HEIGHT));
+        teamView.setTitle(team.getTeamName());
+        teamView.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
+        // ^ prevent user from doing something else
+        teamView.setLocationRelativeTo(null);
+        teamView.setLayout(new BorderLayout());
+
+        showMembers(teamView, team);
+        addTeamOptions(teamView);
+
+        teamView.setVisible(false);
+        teamView.setVisible(true);
     }
 
     // MODIFIES: teamView
     // EFFECTS: adds option buttons for teamView dialog
     private void addTeamOptions(JDialog dialog) {
-        dialog.setLayout(null);
+        JPanel teamOptions = new JPanel();
         JButton buttonAdd = new JButton("Add Hero");
         JButton buttonRem = new JButton("Remove Hero");
         JButton buttonFav = new JButton("Favourite");
-        buttonAdd.setBounds(TEAMVIEW_BUTTON_X,TEAMVIEW_HEIGHT - 100,130,40);
-        buttonRem.setBounds(TEAMVIEW_BUTTON_X + 140,TEAMVIEW_HEIGHT - 100,130,40);
-        buttonFav.setBounds(TEAMVIEW_BUTTON_X + (2 * 140),TEAMVIEW_HEIGHT - 100,130,40);
-        dialog.add(buttonAdd);
-        dialog.add(buttonRem);
-        dialog.add(buttonFav);
+        teamOptions.add(buttonAdd);
+        teamOptions.add(buttonRem);
+        teamOptions.add(buttonFav);
+        dialog.add(teamOptions, BorderLayout.PAGE_END);
 
     }
 
     // MODIFIES: this
-    // EFFECTS: opens up team view for clicked team button
-    private void openTeamView(ActionEvent e, String teamName) {
-        teamView = new JDialog();
-        teamView.setSize(new Dimension(TEAMVIEW_WIDTH,TEAMVIEW_HEIGHT));
-        teamView.setTitle(teamName);
-        teamView.setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
-        // ^ prevent user from doing something else
-        teamView.setLocationRelativeTo(null);
-        addTeamOptions(teamView);
-        teamView.setVisible(true);
+    // EFFECTS: displays members of current team in teamview dialog
+    private void showMembers(JDialog dialog, Team team) {
+        JPanel teamMembers = new JPanel();
+        teamMembers.setLayout(new BoxLayout(teamMembers, BoxLayout.PAGE_AXIS));
+        for (Hero h : team.getTeamMembers()) {
+            JLabel heroLabel = new JLabel("Name: " + h.getHeroGivenName()
+                    + ", Class: " + h.getHeroType().getHeroTypeName());
+            teamMembers.add(heroLabel);
+        }
+        dialog.add(teamMembers);
     }
 
     // MODIFIES: this
     // EFFECTS: adds new team button
-    // TODO: placeholder method
+    // TODO: placeholder method; add ability to name new team immediately
     private void addTeam(ActionEvent e) {
         JButton newTeamButton = new JButton("New Team");
-        panel.add(newTeamButton);
-        panel.setVisible(false);
-        panel.setVisible(true);
+        teamPanel.add(newTeamButton);
+        teamPanel.setVisible(false);
+        teamPanel.setVisible(true);
     }
 
     // MODIFIES: this
     // EFFECTS: removes last team button
-    // TODO: placeholder method
+    // TODO: placeholder method; add ability to click on team button that should be removed
     private void removeTeam(ActionEvent e) {
-        JButton newTeamButton = new JButton("New Team");
-        panel.remove(newTeamButton);
-        panel.setVisible(false);
-        panel.setVisible(true);
+        if (teamPanel.getComponentCount() > 0) {
+            teamPanel.remove(teamPanel.getComponentCount() - 1);
+        }
+        teamPanel.setVisible(false);
+        teamPanel.setVisible(true);
+    }
+
+    private void loadTeams(ActionEvent e) {
+        try {
+            savedTeams = jsonReader.read();
+            System.out.println("Loaded all teams from " + JSON_STORE);
+        } catch (IOException exc) {
+            System.out.println("Unable to read from file: " + JSON_STORE);
+        }
+    }
+
+    private void saveTeams(ActionEvent e) {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(savedTeams);
+            jsonWriter.close();
+            System.out.println("Saved all teams to " + JSON_STORE);
+        } catch (FileNotFoundException exc) {
+            System.out.println("Unable to write to file: " + JSON_STORE);
+        }
     }
 
 }
